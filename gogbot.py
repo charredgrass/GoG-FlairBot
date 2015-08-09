8f2e0bfd30006c5c398494bef2daf0657167517b
Maxwell Chow <maxchowla@gmail.com>
2015-08-08 21:19:03 -0700

    Update gogbot.py

--- gogbot.py
+++ gogbot.py
@@ -9,6 +9,8 @@ def main():
 	me = r.get_redditor('CharredBot')
 	subreddit = r.get_subreddit("charredgrass")
 
+	confirm_thread = r.get_submission(submission_id="3g7ybp") #thread where mods confirm gifted
+
 	GRABBED_REGEX = re.compile("Grabbed ([0-9]*)")
 
 
@@ -25,6 +27,7 @@ def main():
 						rip = True
 				if not rip:
 					give_flair(commie)
+		#next, check confirm_thread
 
 #Comment Format
 
@@ -49,29 +52,36 @@ def give_flair(com):
 			flair_gifted = "Gifted" in flair
 			flair_grabbed = "Grabbed" in flair
 			amt_grabbed = None
-			if flair_gifted and False: #something to check if user doesn't already have flair_gifted
-				pass
-				#verify with mods
+			# if flair_gifted and not is_user_gifted(com.author): #see if user is going from non-gifted to gifted
+			# 	print("confirming with mods")
+			# 	com.reply("You are upgrading your flair to Gifted, this requires moderator confirmation. Just wait, and your flair will be upgraded when the moderators get to it.")
+			# 	#Here it should get the perma of 'com'. Reply with author's name, flair needed, and the perma, on 'confirm_thread'. Then it has to check that to see if the mods have done shit.
+			# 	confirm_thread.reply(com.permalink + "\n\n" + com.author + " wants " + flair + " flair. Please verify.")
+			# 	#TODO this
 			if flair_grabbed:
 				amt_grabbed = re.search(GRABBED_REGEX,flair).group(0)
 			if flair_gifted and not flair_grabbed and not user_has_custom_flair(com.author):
 				#check if user has a custom flair. no praw method to do it, check json maybe
 				r.set_flair(subreddit,com.author,"Gifted","gifted")
 				print "gifted"
-				com.reply("flair set")
+				com.reply(get_response(flair))
 			if flair_grabbed and not flair_gifted and not user_has_custom_flair(com.author):
 				r.set_flair(subreddit,com.author,"" + amt_grabbed, "grabbed")
 				print "grabbed"
-				com.reply("flair set")
+				com.reply(get_response(flair))
 			if flair_grabbed and flair_gifted and not user_has_custom_flair(com.author):
 				r.set_flair(subreddit,com.author,"Gifted | " + amt_grabbed, "giftedgrabbed")
 				print "giftedgrabbed"
-				com.reply("flair set")
+				com.reply(get_response(flair))
 			if user_has_custom_flair(com.author):
 				r.set_flair(subreddit,com.author,flair,get_user_cust_flair_class(com.author))
 				print "custom"
-				com.reply("flair set")
+				com.reply(get_response(flair))
 		else:
+			try:
+				com.reply("I think something went wrong. Make sure you've got the format correct, and contact \/u/charredgrass if necessary.")
+			except:
+				print("um")
 			return False
 	else:
 		return False
@@ -84,6 +94,14 @@ def user_has_custom_flair(person):
 
 def get_user_cust_flair_class(person):
 	return r.get_flair(subreddit,person).__getitem__(u'flair_css_class')
+
+def is_user_gifted(person):
+	return not not u'Gifted' in r.get_flair(subreddit,person).__getitem__(u'flair_text')
+
+def get_response(flair):
+	resp = "Setting flair to **" + flair + "**. Hopefully."
+	resp = resp + "\n\n" + "Contact \/u/charredgrass with any problems." + "\n\n" + "---\n\nI'm just a poor bot. Pleas don't hurt me."
+	return resp
 
 if __name__ == '__main__':
 	main()
